76dabfb6f7e2f3bd483adc74ad5bc9c39859c8fa
Sarwan Hakm <sarwan.h.najim@gmail.com>
2021-11-17 16:33:00 +0300

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            var apiResponse = await apiClient.GetAsync("https://localhost:6001/identity");
+            if (!apiResponse.IsSuccessStatusCode)
+                Console.WriteLine(apiResponse.StatusCode);
+            else
+            {
+                var content = await apiResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/IdentityController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApi.Controllers
+{
+    [Route("identity")]
+    [ApiController]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Employee> Get()
+        {
+            //return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+            var employees= new List<Employee>
+            {
+                new Employee
+                {
+                    Id=1,
+                    Name="Sarwan Hakm",
+                    Position="Dotnet Developer"
+                },
+                new Employee
+                {
+                    Id=2,
+                    Name="Sarwan Hakm",
+                    Position="Api Developer"
+                },
+                new Employee
+                {
+                    Id=3,
+                    Name="Sarwan Hakm",
+                    Position="C# Developer"
+                }
+            };
+            return Ok(employees);
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+}
